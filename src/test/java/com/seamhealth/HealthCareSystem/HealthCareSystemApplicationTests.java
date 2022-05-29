1957ef14a1607726459e5b085113b80c411297e7
Timosamedet <Timosamedet@Compactness>
2022-05-29 18:09:46 +0100

    first commit....

---
+++ src/test/java/com/seamhealth/HealthCareSystem/HealthCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamhealth.HealthCareSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
